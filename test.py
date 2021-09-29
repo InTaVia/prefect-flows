2e89476e49dfb832c41f40d92f618f555187d80e
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2021-09-29 20:35:51 +0200

    test flow

---
+++ test.py
@@ -0,0 +1,34 @@
+i"""
+# Parameters
+
+A `Parameter` is a special type of `Task` representing an input that can vary
+per flow run. For example:
+
+```python
+x = Parameter("x", default=1)
+```
+
+Parameters have a name (`"x"` in this case), and may optionally include a
+default value. Parameters lacking a default value require an explicit value be
+configured for each flow run. Parameters with a default value may use the
+default, or optionally provide a different value at runtime. Parameters can be
+specified through the UI or CLI when running with Prefect Cloud/Server (see
+[here](/orchestration/tutorial/parameters.md)) or through the `parameters`
+kwarg when running locally with `flow.run`.
+
+For more information, see the [Parameter docs](/core/concepts/parameters.md).
+"""
+
+from prefect import Flow, Parameter, task
+
+
+@task(log_stdout=True)
+def print_total(x, y, total):
+    print(f"{x} + {y} = {total}")
+
+
+with Flow("Example: Parameters") as flow:
+    x = Parameter("x", default=1)
+    y = Parameter("y", default=2)
+
+    print_total(x, y, x + y)
