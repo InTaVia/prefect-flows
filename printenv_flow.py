2cb0678877ead09cc7d92054d07fed37f325a42a
Kesäniemi Joonas <joonas.kesaniemi@aalto.fi>
2021-10-13 18:08:25 +0300

    Added flow that just prints envs

---
+++ printenv_flow.py
@@ -0,0 +1,15 @@
+from prefect import Flow, Parameter, task
+from prefect.storage import GitHub
+import os
+
+@task(log_stdout=True)
+def print_envs():
+    for item, value in os.environ.items():
+        print('{}: {}'.format(item, value))
+    
+
+
+with Flow("Print envs") as flow:
+    print_envs()
+
+flow.Storage = GitHub(repo="InTaVia/prefect-flows", path="printevn_flow.py")
