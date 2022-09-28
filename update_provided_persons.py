92e8862bbccd988b36b0c0f3b770957ac6dfd2cb
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2022-09-28 13:54:19 +0200

    added auth again

--- update_provided_persons.py
+++ update_provided_persons.py
@@ -1,4 +1,5 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
+from requests.auth import HTTPBasicAuth
 from rdflib import URIRef, Namespace, Graph
 from rdflib.namespace import OWL, RDF
 import prefect
@@ -13,16 +14,17 @@ IDMCORE = Namespace("http://www.intavia.eu/idm-core/")
 
 @task(log_stdout=True)
 def setup_sparql_connection(endpoint):
-    sparql = SPARQLWrapper(endpoint)    
+    sparql = SPARQLWrapper(endpoint)
     sparql.setHTTPAuth("BASIC")
     sparql.setCredentials(user=os.environ.get(
         "RDFDB_USER"), passwd=os.environ.get("RDFDB_PASSWORD"))
     return sparql
 
+
 @task
 def load_id_graph(sparql, id_source_graph):
-  logger = prefect.context.get('logger')
-  loadQuery = """  
+    logger = prefect.context.get('logger')
+    loadQuery = """  
   construct {
     ?s ?p ?o
   }
@@ -32,25 +34,27 @@ def load_id_graph(sparql, id_source_graph):
   }
   """
 
-  sparql.setQuery(loadQuery)
-  results = sparql.queryAndConvert()
-  
-  with open('/tmp/lg_data.ttl', 'w') as file:
-    file.write(results.serialize())
-  
-  g = Graph()
-  g.parse('/tmp/lg_data.ttl')
+    sparql.setQuery(loadQuery)
+    results = sparql.queryAndConvert()
 
-  logger.info('Linking data loaded')
+    with open('/tmp/lg_data.ttl', 'w') as file:
+        file.write(results.serialize())
 
-  return g
+    g = Graph()
+    g.parse('/tmp/lg_data.ttl')
+
+    logger.info('Linking data loaded')
+
+    return g
+
 
 @task
 def create_provided_persons_graph(sparql, id_graph, person_source_uris):
-  logger = prefect.context.get('logger')
+    logger = prefect.context.get('logger')
 
-  from_part = '\n'.join(list(map(lambda x : 'from <' + x +'>', person_source_uris) ))
-  personQuery = """
+    from_part = '\n'.join(
+        list(map(lambda x: 'from <' + x + '>', person_source_uris)))
+    personQuery = """
   select ?person
   """ + from_part + """
   where 
@@ -59,66 +63,80 @@ def create_provided_persons_graph(sparql, id_graph, person_source_uris):
   } 
   """
 
-  logger.info(personQuery)
+    logger.info(personQuery)
 
-  g = Graph()
+    g = Graph()
 
-  providedPersonType = IDMCORE.Provided_Person
-  addedPersonProxies = set()
-  
-  providedPersonCount = 1
-  sparql.setQuery(personQuery)
-  sparql.setReturnFormat(JSON)
-  results = sparql.query().convert()
-  for result in results["results"]["bindings"]:
-    personProxyURI = result['person']['value']
-    check = False
-    personProxy = URIRef(personProxyURI)
-    providedPersonURI = URIRef(IDM_PROVIDED_PERSON + str(providedPersonCount))
-    g.add((providedPersonURI, RDF.type, providedPersonType))
-    # check for owl:sameAs links 
-    added = False
-    for extID in id_graph.objects(personProxy, OWL.sameAs):
-      for otherProxy in id_graph.subjects(OWL.sameAs, extID):
-        if not otherProxy in addedPersonProxies:
-          g.add((otherProxy, IDMCORE.person_proxy_for, providedPersonURI))
-          addedPersonProxies.add(otherProxy)
-          added = True
-    if not personProxy in addedPersonProxies:
-      g.add((personProxy, IDMCORE.person_proxy_for, providedPersonURI))
-      addedPersonProxies.add(personProxy)
-      added = True 
+    providedPersonType = IDMCORE.Provided_Person
+    addedPersonProxies = set()
 
-    if added:
-      providedPersonCount = providedPersonCount + 1
-      
-  logger.info('Number of provided persons created: ' + str(providedPersonCount))
+    providedPersonCount = 1
+    sparql.setQuery(personQuery)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    for result in results["results"]["bindings"]:
+        personProxyURI = result['person']['value']
+        check = False
+        personProxy = URIRef(personProxyURI)
+        providedPersonURI = URIRef(
+            IDM_PROVIDED_PERSON + str(providedPersonCount))
+        g.add((providedPersonURI, RDF.type, providedPersonType))
+        # check for owl:sameAs links
+        added = False
+        for extID in id_graph.objects(personProxy, OWL.sameAs):
+            for otherProxy in id_graph.subjects(OWL.sameAs, extID):
+                if not otherProxy in addedPersonProxies:
+                    g.add((otherProxy, IDMCORE.person_proxy_for, providedPersonURI))
+                    addedPersonProxies.add(otherProxy)
+                    added = True
+        if not personProxy in addedPersonProxies:
+            g.add((personProxy, IDMCORE.person_proxy_for, providedPersonURI))
+            addedPersonProxies.add(personProxy)
+            added = True
 
-  #g.serialize(destination="providedPersons.ttl")  
+        if added:
+            providedPersonCount = providedPersonCount + 1
 
-  logger.info('Person provided updated')
-  return g
+    logger.info('Number of provided persons created: ' +
+                str(providedPersonCount))
+
+    # g.serialize(destination="providedPersons.ttl")
+
+    logger.info('Person provided updated')
+    return g
+
 
 @task()
 def update_target_graph(endpoint, target_uri, data):
-  logger = prefect.context.get('logger')
-  logger.info(len(data))
-  delete_url =  endpoint + '?c=<' + target_uri + '>'
-  post_url =  endpoint + '?context-uri=' + target_uri + ''
-  requests.delete(delete_url)
-  requests.post(post_url, headers={'Content-type': 'text/turtle'}, data=data.serialize())
+    logger = prefect.context.get('logger')
+    delete_url =  endpoint + '?c=<' + target_uri + '>'
+    auth = HTTPBasicAuth(os.environ.get("RDFDB_USER"), os.environ.get("RDFDB_PASSWORD"))
+    post_url =  endpoint + '?context-uri=' + target_uri + ''
+    res = requests.delete(delete_url, auth=auth)
+    res2 = requests.post(post_url, headers={'Content-type': 'text/turtle'}, data=data.serialize(), auth=auth)
+    logger.info(res)
+    logger.info(res2)
+    logger.info(len(data))
+
 
 with Flow("Generate provided person graph") as flow:
-  endpoint = Parameter("endpoint", default="https://triplestore.acdh-dev.oeaw.ac.at/intavia/sparql")
-  id_source_uri = Parameter('id_source_uri', default='http://www.intavia.org/graphs/person-id-enrichment') # string
-  person_source_uris = Parameter('person_source_uris', default=['http://www.intavia.org/graphs/apis', 'http://www.intavia.org/graphs/bs']) # list
-  target_graph = Parameter('target_graph', default='http://www.intavia.org/graphs/provided_persons') # string
+    endpoint = Parameter(
+        "endpoint", default="https://triplestore.acdh-dev.oeaw.ac.at/intavia/sparql")
+    id_source_uri = Parameter(
+        'id_source_uri', default='http://www.intavia.org/graphs/person-id-enrichment')  # string
+    person_source_uris = Parameter('person_source_uris', default=[
+                                   'http://www.intavia.org/graphs/apis', 'http://www.intavia.org/graphs/bs'])  # list
+    target_graph = Parameter(
+        'target_graph', default='http://www.intavia.org/graphs/provided_persons')  # string
 
-  sparql = setup_sparql_connection(endpoint)
-  id_graph = load_id_graph(sparql, id_source_uri)
-  provided_persons_graph = create_provided_persons_graph(sparql, id_graph, person_source_uris)
-  update_target_graph(endpoint, target_graph, provided_persons_graph)
+    sparql = setup_sparql_connection(endpoint)
+    id_graph = load_id_graph(sparql, id_source_uri)
+    provided_persons_graph = create_provided_persons_graph(
+        sparql, id_graph, person_source_uris)
+    update_target_graph(endpoint, target_graph, provided_persons_graph)
 
 
-flow.run_config = KubernetesRun(env={"EXTRA_PIP_PACKAGES": "SPARQLWrapper rdflib requests"}, job_template_path="https://raw.githubusercontent.com/InTaVia/prefect-flows/master/intavia-job-template.yaml")
-flow.storage = GitHub(repo="InTaVia/prefect-flows", path="update_provided_persons.py")
+flow.run_config = KubernetesRun(env={"EXTRA_PIP_PACKAGES": "SPARQLWrapper rdflib requests"},
+                                job_template_path="https://raw.githubusercontent.com/InTaVia/prefect-flows/master/intavia-job-template.yaml")
+flow.storage = GitHub(repo="InTaVia/prefect-flows",
+                      path="update_provided_persons.py")
