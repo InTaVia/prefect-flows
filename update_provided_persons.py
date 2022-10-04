47e2bef9999fee2e5bbe6c4f38c1ec59f60d47d6
Kesäniemi Joonas <joonas.kesaniemi@aalto.fi>
2022-10-04 09:56:47 +0300

    Added first implementation for generating provenance

--- update_provided_persons.py
+++ update_provided_persons.py
@@ -1,16 +1,23 @@
+from venv import create
 from SPARQLWrapper import SPARQLWrapper, JSON
 from requests.auth import HTTPBasicAuth
-from rdflib import URIRef, Namespace, Graph
-from rdflib.namespace import OWL, RDF
+from rdflib import URIRef, Namespace, Graph, Literal, XSD
+from rdflib.namespace import OWL, RDF, RDFS
 import prefect
 from prefect import Flow, task, Parameter
 from prefect.storage import GitHub
 from prefect.run_configs import KubernetesRun
 import requests
 import os
+from string import Template
+import datetime
+
 IDM_PROVIDED_PERSON = 'http://www.intavia.eu/provided_person/'
 IDMCORE = Namespace("http://www.intavia.eu/idm-core/")
-
+IDM_PROV = Namespace("http://www.intavia.eu/idm-prov/")
+IDM_PREFECT = Namespace("http://www.intavia.eu/idm-prefect/")
+PROV = Namespace("http://www.w3.org/ns/prov#")
+PROV_TARGET_GRAPH = 'http://www.intavia.org/graphs/provenance'
 
 @task(log_stdout=True)
 def setup_sparql_connection(endpoint):
@@ -117,8 +124,98 @@ def update_target_graph(endpoint, target_uri, data):
     logger.info(res)
     logger.info(res2)
     logger.info(len(data))
+    return True
 
 
+@task(log_stdout=True)
+def get_start_time():
+    schedule_time = prefect.context.get("scheduled_start_time")
+    if schedule_time:
+        return schedule_time
+    else:
+        datetime.now()
+
+def create_source_entities(): 
+    entities = []
+    flow_run_id = prefect.context.flow_run_id
+    params = prefect.context.parameters
+    g = Graph()
+    for index, source_graph in enumerate(params['person_source_uris']):
+
+        source_entity = URIRef(IDM_PROV[flow_run_id + '/source/' + str(index)])
+        
+        g.add( (source_entity, RDF.type, PROV.Entity) )
+        g.add( (source_entity, IDM_PROV.source_graph, URIRef(source_graph)))
+        entities.append(source_entity)
+
+    # id_source_uri
+    id_source = URIRef(IDM_PROV[flow_run_id + '/source/id_source'])
+    g.add( (id_source, RDF.type, PROV.Entity) )
+    g.add( (id_source, IDM_PROV.source_graph, URIRef(params['id_source_uri'])))
+    entities.append(source_entity)
+
+    return entities, g
+
+def create_target_entities():
+    entities = []
+    flow_run_id = prefect.context.flow_run_id
+    params = prefect.context.parameters
+    g = Graph()
+
+    target_graph = URIRef(IDM_PROV[flow_run_id + '/target'])
+    g.add( (target_graph, RDF.type, PROV.Entity) )
+    g.add( (target_graph, IDM_PROV.target_graph, URIRef(params['target_graph'])))
+    entities.append(target_graph)
+
+    return entities, g   
+
+@task()
+def add_provenance(_, start_time, create_source_entities, create_target_entities, endpoint,t2,t3,t4):
+    logger = prefect.context.get('logger')
+    
+    flow_name = prefect.context.flow_name
+    flow_id = prefect.context.flow_id
+    flow_run_id = prefect.context.flow_run_id    
+    flow_run_version = prefect.context.get('flow_run_version', 'not-available')
+    end_time = datetime.datetime.now()
+    
+
+    g = Graph()
+
+    
+    activityURI = URIRef(IDM_PROV['activity/' + flow_run_id])
+    g.add((activityURI, RDF.type, PROV.Activity))
+    g.add((activityURI, IDM_PREFECT.flow_name, Literal(flow_name)))
+    g.add((activityURI, IDM_PREFECT.flow_id, Literal(flow_id)))
+    g.add((activityURI, IDM_PREFECT.flow_run_version, Literal(flow_run_version)))
+    g.add((activityURI, PROV.startedAtTime, Literal(start_time.isoformat(), datatype=XSD.dateTime)))
+    g.add((activityURI, PROV.endedAtTime, Literal(end_time.isoformat(), datatype=XSD.dateTime)))
+    # used 
+    source_entities, source_entity_graph = create_source_entities()
+    g = g + source_entity_graph
+    for source_entity in source_entities:
+        g.add( (activityURI, PROV.used, source_entity))
+
+
+    # generated 
+    target_entities, target_entity_graph = create_target_entities()
+    g = g + target_entity_graph
+    for target_entity in target_entities:
+        g.add( (activityURI, PROV.generated, target_entity))
+
+    print(g.serialize())
+
+    
+    auth = HTTPBasicAuth(os.environ.get("RDFDB_USER"), os.environ.get("RDFDB_PASSWORD"))
+    post_url =  endpoint + '?context-uri=' + PROV_TARGET_GRAPH + ''
+    res2 = requests.post(post_url, headers={'Content-type': 'text/turtle'}, data=g.serialize(), auth=auth)
+    logger.info(res2)
+  #with open('sparql/insert_flow_run_provenance.sparql', "r+") as template_file:
+  #  insert = Template(template_file.read()).substitute(
+  #      named_graph = TARGET_GRAPH,
+#      )
+
+ 
 with Flow("Generate provided person graph") as flow:
     endpoint = Parameter(
         "endpoint", default="https://triplestore.acdh-dev.oeaw.ac.at/intavia/sparql")
@@ -129,14 +226,23 @@ with Flow("Generate provided person graph") as flow:
     target_graph = Parameter(
         'target_graph', default='http://www.intavia.org/graphs/provided_persons')  # string
 
+    start_time = get_start_time()
     sparql = setup_sparql_connection(endpoint)
     id_graph = load_id_graph(sparql, id_source_uri)
     provided_persons_graph = create_provided_persons_graph(
         sparql, id_graph, person_source_uris)
-    update_target_graph(endpoint, target_graph, provided_persons_graph)
+    res = update_target_graph(endpoint, target_graph, provided_persons_graph)
+    add_provenance(res, start_time, create_source_entities, create_target_entities, endpoint, person_source_uris,target_graph,id_source_uri)
 
 
 flow.run_config = KubernetesRun(env={"EXTRA_PIP_PACKAGES": "SPARQLWrapper rdflib requests"},
                                 job_template_path="https://raw.githubusercontent.com/InTaVia/prefect-flows/master/intavia-job-template.yaml")
 flow.storage = GitHub(repo="InTaVia/prefect-flows",
                       path="update_provided_persons.py")
+
+# flow.run(
+#     endpoint='http://localhost:9999/blazegraph/namespace/intavia/sparql',
+#     person_source_uris=['http://www.intavia.org/graphs/apis', 'http://www.intavia.org/graphs/bs'],
+#     target_graph='http://www.intavia.org/graphs/provided_persons',
+#     id_source_uri='http://www.intavia.org/graphs/person-id-enrichment'
+# )
