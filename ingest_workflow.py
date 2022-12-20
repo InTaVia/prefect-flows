ff8dc2d7212736ef732645fc0bde1054f1570d83
Joonas Kesäniemi <joonas.kesaniemi@csc.fi>
2022-12-20 14:48:39 +0200

    Added source type param and auth headers

--- ingest_workflow.py
+++ ingest_workflow.py
@@ -9,14 +9,13 @@ import requests
 import os
 
 @task
-def add_data_from_url(endpoint, source_url, target_graph_uri):
+def add_data_from_url(endpoint, source_url, source_type, target_graph_uri):
     logger = prefect.context.get('logger')
     res = requests.get(source_url)
-    #auth = HTTPBasicAuth(os.environ.get("RDFDB_USER"), os.environ.get("RDFDB_PASSWORD"))
+    auth = HTTPBasicAuth(os.environ.get("RDFDB_USER"), os.environ.get("RDFDB_PASSWORD"))
     post_url =  endpoint + '?context-uri=' + target_graph_uri + ''
     logger.info(post_url)
-    #res = requests.post(post_url, headers={'Content-type': 'text/turtle'}, data=f, auth=auth)
-    res = requests.post(post_url, headers={'Content-type': 'text/turtle'}, data=res.text.encode('utf-8'))
+    res = requests.post(post_url, headers={'Content-type': source_type}, data=res.text.encode('utf-8'), auth=auth)
     logger.info(res)
 
     return True
@@ -33,9 +32,11 @@ with Flow("Ingest workflow") as flow:
         "target_graph_id")
     target_label = Parameter(
         "target_label")
-
+    source_type = Parameter(
+        "source_type")
+    
     target_named_graph_uri = generate_versioned_named_graph(target_graph_id)
-    res = add_data_from_url(endpoint, source_url, target_named_graph_uri)
+    res = add_data_from_url(endpoint, source_url, source_type, target_named_graph_uri)
     add_provenance_data(res, 
         endpoint,
         [
@@ -60,6 +61,7 @@ flow.storage = GitHub(repo="InTaVia/prefect-flows", path="ingest_workflow.py")
 # flow.run(
 #     endpoint='http://localhost:9999/blazegraph/namespace/intavia/sparql',
 #     source_url='https://raw.githubusercontent.com/InTaVia/idm-rdf/main/test_dataset_designsprint/exdataset.ttl',
+#     source_type='text/turtle',
 #     source_label='Test dataset',
 #     target_graph_id='http://intavia.eu/graphs/test_dataset',
 #     target_label='Test data'
