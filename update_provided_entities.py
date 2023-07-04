817fd9c4e07c43abd31f249a4dd81acd22349ba4
Jouni Tuominen <jouni.tuominen@aalto.fi>
2023-07-04 16:29:55 +0300

    take into account 1) existing provided_entities in source data (create new provided_entity instances for them), and 2) entity proxies that don't have sameAs links to external sources (and are not connected to existing provided_entities in source data)

--- update_provided_entities.py
+++ update_provided_entities.py
@@ -58,11 +58,83 @@ def get_sameas_statements(sparql, entity_source_uris, entity_source_type, entity
     logger.info('sameAs statements: ' + str(len(results)))
     return g
 
-
+# get provided entities that already exist in source data (with entity proxies of which none has sameAs links to external sources)
 @task
-def create_provided_entities_graph(sparql, id_graph, entity_enriched_uris, entity_source_type, entity_source_proxy_type, provided_entity_ns, provided_entity_type, entity_proxy_for_property):
+def get_existing_provided_entities_with_unmapped_proxies(sparql, entity_source_uris, provided_entity_type, entity_proxy_for_property):
     logger = prefect.context.get('logger')
 
+    from_part = '\n'.join(
+        list(map(lambda x: 'from <' + x + '>', entity_source_uris)))
+    entityQuery = """
+    PREFIX owl: <http://www.w3.org/2002/07/owl#>
+    SELECT *
+    """ + from_part + """
+    WHERE {
+      ?provided a <""" + provided_entity_type + """> .
+      ?proxy <""" + entity_proxy_for_property + """> ?provided .
+      FILTER NOT EXISTS {
+        ?proxy2 <""" + entity_proxy_for_property + """> ?provided ;
+                owl:sameAs ?ext_id .
+      }
+    }
+    """
+
+    sparql.setQuery(entityQuery)
+    sparql.setReturnFormat(JSON)
+    results = sparql.queryAndConvert()
+
+    provided_entities = dict()
+    for result in results["results"]["bindings"]:
+        provided_entity = result['provided']['value']
+        entity_proxy = result['proxy']['value']
+        if provided_entity not in provided_entities:
+            provided_entities[provided_entity] = list()
+        provided_entities[provided_entity].append(entity_proxy)
+
+    logger.info('Queried existing provided entities with no mapped proxy entities')
+    logger.info('provided entity count: ' + str(len(provided_entities)))
+    return provided_entities
+
+# get entity proxies that don't have sameAs links to external sources (and aren't connected to existing provided entities in source data)
+@task
+def get_unmapped_proxies_without_existing_provided_entities(sparql, entity_source_uris, entity_source_type, entity_source_proxy_type, entity_proxy_for_property):
+    logger = prefect.context.get('logger')
+
+    from_part = '\n'.join(
+        list(map(lambda x: 'from <' + x + '>', entity_source_uris)))
+    entityQuery = """
+    PREFIX owl: <http://www.w3.org/2002/07/owl#>
+    SELECT DISTINCT ?entity
+    """ + from_part + """
+    WHERE {
+    VALUES ?class {<""" + entity_source_type + """> <""" + entity_source_proxy_type + """>}
+    ?entity a ?class .
+    FILTER NOT EXISTS {
+        ?entity <""" + entity_proxy_for_property + """> ?provided ;
+    }
+    FILTER NOT EXISTS {
+        ?entity owl:sameAs ?ext_id .
+    }
+    }
+    """
+
+    sparql.setQuery(entityQuery)
+    sparql.setReturnFormat(JSON)
+    results = sparql.queryAndConvert()
+
+    entity_proxies = list()
+    for result in results["results"]["bindings"]:
+        entity_proxies.append(result['entity']['value'])
+
+    logger.info('Queried unmapped entity proxies without existing provided entities')
+    logger.info('entity proxy count: ' + str(len(entity_proxies)))
+    return entity_proxies
+
+@task
+def create_provided_entities_graph(sparql, id_graph, entity_enriched_uris, entity_source_type, entity_source_proxy_type, provided_entity_ns, provided_entity_type, entity_proxy_for_property, provided_entities, entity_proxies):
+    logger = prefect.context.get('logger')
+
+    # 1) entities that have sameAs links to external sources (e.g. Wikidata)
     entityQuery = """
   SELECT DISTINCT ?entity
   FROM <""" + entity_enriched_uris + """>
@@ -110,6 +182,31 @@ def create_provided_entities_graph(sparql, id_graph, entity_enriched_uris, entit
     logger.info('Number of provided entities created: ' +
                 str(providedEntityCount))
 
+    # 2a) existing provided entities with entity proxies of which none has sameAs links to external sources
+    for entityProxies in provided_entities.values():
+        providedEntityURI = URIRef(
+            provided_entity_ns + str(providedEntityCount))
+        g.add((providedEntityURI, RDF.type, providedEntityTypeURI))
+        for entityProxy in entityProxies:
+            entityProxyURI = URIRef(entityProxy)
+            g.add((entityProxyURI, entityProxyForPropertyURI, providedEntityURI))
+        providedEntityCount = providedEntityCount + 1
+
+    logger.info('Number of provided entities created (incl. existing, non-mapped ones): ' +
+                str(providedEntityCount))
+
+    # 2b) entity proxies that don't have sameAs links to external sources and aren't connected to existing provided entities
+    for entityProxy in entity_proxies:
+        providedEntityURI = URIRef(
+            provided_entity_ns + str(providedEntityCount))
+        g.add((providedEntityURI, RDF.type, providedEntityTypeURI))
+        entityProxyURI = URIRef(entityProxy)
+        g.add((entityProxyURI, entityProxyForPropertyURI, providedEntityURI))
+        providedEntityCount = providedEntityCount + 1
+
+    logger.info('Number of provided entities created (incl. for non-mapped proxies without existing ones): ' +
+                str(providedEntityCount))
+
     # g.serialize(destination="providedEntities.ttl")
 
     logger.info('Entity provided updated')
@@ -118,6 +215,7 @@ def create_provided_entities_graph(sparql, id_graph, entity_enriched_uris, entit
 
 @task()
 def update_target_graph(endpoint, target_uri, data):
+    #return
     logger = prefect.context.get('logger')
     delete_url =  endpoint + '?c=<' + target_uri + '>'
     auth = HTTPBasicAuth(os.environ.get("RDFDB_USER"), os.environ.get("RDFDB_PASSWORD"))
@@ -173,6 +271,7 @@ def create_target_entities():
 
 @task()
 def add_provenance(_, start_time, create_source_entities, create_target_entities, endpoint):
+    #return
     logger = prefect.context.get('logger')
     
     flow_name = prefect.context.flow_name
@@ -215,7 +314,7 @@ def add_provenance(_, start_time, create_source_entities, create_target_entities
 with Flow("Generate provided entity graph") as flow:
     endpoint = Parameter(
         "endpoint", default="https://triplestore.acdh-dev.oeaw.ac.at/intavia/sparql") # string
-    entity_source_uris = Parameter('entity_source_uris', default=['https://apis.acdh.oeaw.ac.at/data', 'http://ldf.fi/nbf/data', 'http://data.biographynet.nl', 'http://data.biographynet.nl/places2wikidata/', 'http://www.intavia.eu/sbi', 'http://www.intavia.org/graphs/person-id-enrichment'])  # list
+    entity_source_uris = Parameter('entity_source_uris', default=['http://apis.acdh.oeaw.ac.at/data/v5', 'http://ldf.fi/nbf/data', 'http://data.biographynet.nl', 'http://www.intavia.eu/sbi', 'http://www.intavia.org/graphs/person-id-enrichment'])  # list
     entity_source_type = Parameter(
         "entity_source_type", default="http://www.cidoc-crm.org/cidoc-crm/E21_Person") # string
     entity_source_proxy_type = Parameter(
@@ -234,8 +333,10 @@ with Flow("Generate provided entity graph") as flow:
     start_time = get_start_time()
     sparql = setup_sparql_connection(endpoint)
     id_graph = get_sameas_statements(sparql, entity_source_uris, entity_source_type, entity_source_proxy_type)
+    provided_entities = get_existing_provided_entities_with_unmapped_proxies(sparql, entity_source_uris, provided_entity_type, entity_proxy_for_property)
+    entity_proxies = get_unmapped_proxies_without_existing_provided_entities(sparql, entity_source_uris, entity_source_type, entity_source_proxy_type, entity_proxy_for_property)
     provided_entities_graph = create_provided_entities_graph(
-        sparql, id_graph, entity_enriched_uris, entity_source_type, entity_source_proxy_type, provided_entity_ns, provided_entity_type, entity_proxy_for_property)
+        sparql, id_graph, entity_enriched_uris, entity_source_type, entity_source_proxy_type, provided_entity_ns, provided_entity_type, entity_proxy_for_property, provided_entities, entity_proxies)
     res = update_target_graph(endpoint, target_graph, provided_entities_graph)
     add_provenance(res, start_time, create_source_entities, create_target_entities, endpoint)
 
@@ -252,7 +353,7 @@ flow.storage = GitHub(repo="InTaVia/prefect-flows",
 #flow.run(
 #    endpoint='http://localhost:9999/blazegraph/namespace/intavia/sparql',
 #    #endpoint='https://triplestore.acdh-dev.oeaw.ac.at/intavia/sparql',
-#    entity_source_uris=['https://apis.acdh.oeaw.ac.at/data', 'http://ldf.fi/nbf/data', 'http://data.biographynet.nl', 'http://data.biographynet.nl/places2wikidata/', 'http://www.intavia.eu/sbi', 'http://www.intavia.org/graphs/person-id-enrichment'],
+#    entity_source_uris=['http://apis.acdh.oeaw.ac.at/data/v5', 'http://ldf.fi/nbf/data', 'http://data.biographynet.nl', 'http://www.intavia.eu/sbi', 'http://www.intavia.org/graphs/person-id-enrichment'],
 #    entity_source_type="http://www.cidoc-crm.org/cidoc-crm/E21_Person",
 #    entity_source_proxy_type="http://www.intavia.eu/idm-core/Person_Proxy",
 #    entity_enriched_uris="http://www.intavia.org/graphs/person-id-enrichment",
@@ -266,7 +367,7 @@ flow.storage = GitHub(repo="InTaVia/prefect-flows",
 #flow.run(
 #    endpoint='http://localhost:9999/blazegraph/namespace/intavia/sparql',
 #    #endpoint='https://triplestore.acdh-dev.oeaw.ac.at/intavia/sparql',
-#    entity_source_uris=['https://apis.acdh.oeaw.ac.at/data', 'http://ldf.fi/nbf/data', 'http://data.biographynet.nl', 'http://data.biographynet.nl/places2wikidata/', 'http://www.intavia.eu/sbi', 'http://www.intavia.org/graphs/place-id-enrichment'],
+#    entity_source_uris=['http://apis.acdh.oeaw.ac.at/data/v5', 'http://ldf.fi/nbf/data', 'http://data.biographynet.nl', 'http://data.biographynet.nl/places2wikidata/', 'http://www.intavia.eu/sbi', 'http://www.intavia.org/graphs/place-id-enrichment'],
 #    entity_source_type="http://www.cidoc-crm.org/cidoc-crm/E53_Place",
 #    entity_source_proxy_type="http://www.intavia.eu/idm-core/Place_Proxy",
 #    entity_enriched_uris="http://www.intavia.org/graphs/place-id-enrichment",
